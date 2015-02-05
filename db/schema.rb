64fe5792c6938a39d41318c8f0c51462427dc25b
ChrisCPO <ChrisCPO@gmail.com>
2015-02-05 18:28:21 -0500

    added username to users
    * created monban congif file

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 20150203171722) do
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
     t.string   "password_digest", null: false
+    t.string   "username",        null: false
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
   end
